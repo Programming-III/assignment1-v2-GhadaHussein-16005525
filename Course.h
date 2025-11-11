df7afd8432eec6d7b34ebee1aab242ae95c0674c
GhadaHussein-16005525 <ghadaheatham7882@gmail.com>
2025-11-11 09:47:09 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -6,16 +6,29 @@
 using namespace std;
 
 class Course{
-  string courseCode;
-  string courseName;
-  int maxStudents;
-  Student* students;
-  int currentStudents;
-  public:
-  Course(){
-  }
-  Course(string code, string name, int max, Student* s, int curr){
-    
-  }
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+    public:
+    Course(){
+        courseCode = "";
+        courseName = "";
+        maxStudents = 0;
+        currentStudents = 0;
+    }
+    Course(string code, string name, int max, Student* s, int curr){
+        courseCode = code;
+        courseName = name;
+        maxStudents = max;
+        students = s;
+        currentStudents = curr;
+    }
+    ~Course(){
+        delete[] students;
+    }
+    void addStudent(const Student& s);
+    void displayCourseInfo();
 };
 #endif
