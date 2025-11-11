8bd889aad1e58b7cfb71d275c3005b134a7bc76d
GhadaHussein-16005525 <ghadaheatham7882@gmail.com>
2025-11-11 09:52:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,45 +4,28 @@
 #include "Student.h"
 #include "Instructor.h"
 #include "Course.h"
-
-
 using namespace std;
-
-// ==================== Person Class Implementation =========================
-
-
-
-
-
-
-
-
-// ==================== Student Class Implementation ====================
-
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
-int main() {
-   
-    
+void Person :: display(){
+    cout<<"Name: "<<name<<endl<<"ID: "<<id<<endl;
+}
+void Student :: display(){
+    cout<<"Student info: "<<endl;
+    cout<<"Name: "<<getName()<<endl<<"Year: "<<yearLevel<<endl;
+}
+void Instructor :: display(){
+    cout<<"Instructor info: "<<endl;
+    cout<<"Name: "<<getName()<<endl<<"Department: "<<department<<endl<<"Experience: "<<experienceYears<<endl;
+}
+void Course :: displayCourseInfo(){
+    cout<<"Course: "<<courseCode<<" - "<<courseName<<endl<<"Max students: "<<maxStudents<<endl<<
+    "Currently Enrolled: ";
+}
+int main(){
+    Student s;
+    Instructor i;
+    Course c;
+    s.display();
+    i.display();
+    c.displayCourseInfo();
     return 0;
 }
